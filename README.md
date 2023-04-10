77914bac2feb8e6bf4ade68c45d18a0443cd913c
gabrieldesousaalmeida <74242647+gabrieldesousaalmeida@users.noreply.github.com>
2023-04-10 09:25:22 -0300

    renderizando página HTML

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # criando-roteamento-com-express
 roteamento básico com express
+
+renderizando página html
+
+# npm install
+para instalar dependencias
+
+
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res)=>{
     res.send('Olá mundo');
 })
 
+app.get('/blog', (req, res)=>{
+    //__dirname aponta diretorio atual
+    res.sendFile(__dirname + '/src/blog.html');
+})
+
 app.get('/sobre', (req, res)=>{
     res.send('Página Sobre');
 })
